bc737739ae9b9abef1cec54054139a5fd1a7c7ed
Johannes Kantz <67144859+JohannesKantz@users.noreply.github.com>
2024-01-26 20:01:18 +0100

    remove: neon connection pool

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,9 @@
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import ws from "ws";
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-    neonConfig.webSocketConstructor = ws;
-    const connectionString = `${process.env.DATABASE_URL}`;
-
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaNeon(pool);
-    prisma = new PrismaClient({ adapter });
+    prisma = new PrismaClient();
 } else {
     let globalWithPrisma = global as typeof globalThis & {
         prisma: PrismaClient;
