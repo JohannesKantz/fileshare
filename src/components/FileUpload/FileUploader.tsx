e4add893bb3b790799d5c8c517e55e39ea64c297
Johannes Kantz <67144859+JohannesKantz@users.noreply.github.com>
2024-01-26 10:28:40 +0100

    add: File max size

--- src/components/FileUpload/FileUploader.tsx
+++ src/components/FileUpload/FileUploader.tsx
@@ -19,6 +19,10 @@ import { Progress } from "../ui/progress";
 import FileDropzone from "./FileDropzone";
 import FileUploadForm from "./FileUploadForm";
 import { AxiosResponse } from "axios";
+import { useToast } from "../ui/use-toast";
+
+const maxFileSize = 1024 * 1024 * 1024 * 1;
+const maxFileSizeString = formatFileSize(maxFileSize);
 
 export default function FileUploader() {
     const [file, setFile] = React.useState<File>();
@@ -27,6 +31,7 @@ export default function FileUploader() {
     );
 
     const { upload } = useFileUpload();
+    const { toast } = useToast();
 
     const resetFileUpload = () => {
         setFile(undefined);
@@ -34,12 +39,32 @@ export default function FileUploader() {
     };
 
     const onDrop = React.useCallback((acceptedFiles: any) => {
-        console.log({ acceptedFiles });
         setFile(acceptedFiles[0]);
     }, []);
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         onDrop,
+        maxSize: maxFileSize,
+        multiple: false,
+        maxFiles: 1,
+        onDropRejected: (fileRejections: FileRejection[]) => {
+            if (fileRejections[0].errors[0].code === "file-too-large") {
+                toast({
+                    title: "File rejected",
+                    description: `File is too large. Max file size is ${maxFileSizeString}.`,
+                });
+            } else if (fileRejections[0].errors[0].code === "too-many-files") {
+                toast({
+                    title: "Files rejected",
+                    description: "You can only upload one file at a time.",
+                });
+            } else {
+                toast({
+                    title: "File rejected",
+                    description: "An error occured while uploading the file.",
+                });
+            }
+        },
     });
 
     return (
